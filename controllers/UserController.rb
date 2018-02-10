eb5fd58227054f709fd1b3dfc7b532fc5c99751a
sergio <scrummish@gmail.com>
2018-02-10 14:54:30 -0600

    Pre merge commit

--- controllers/UserController.rb
+++ controllers/UserController.rb
@@ -12,24 +12,39 @@ class UserController < ApplicationController
 		resp.to_json
 	end
 
+	post '/login' do
+		payload = params
+		payload = JSON.parse(request.body.read).symbolize_keys
+
+		@pw = payload[:password]
+
+		@user = User.find_by(email: payload[:email])
+		if @user && @user.authenticate(@pw)
+			session[:logged_in] = true
+			session[:username] = @user.firstname
+			session[:user_id] = @user.id
+			session[:message] = "Logged in as #{@user.firstname}"
+		else
+			session[:message] = "Invalid username or password"
+		end
+		session.to_json
+	end
+
 	post '/' do
 		payload = params
 		payload = JSON.parse(request.body.read).symbolize_keys
 
-		p payload
-		p params
 		@user = User.new
 		@user.firstname = payload[:firstname]
 		@user.lastname = payload[:lastname]
 		@user.email = payload[:email]
 		@user.password = payload[:password]
 		@user.save
-		# session[:logged_in] = true
-		# session[:username] = @user.firstname
-		# session[:user_id] = @user.id
-		# session[:message] = "Welcome!"
+		session[:logged_in] = true
+		session[:username] = @user.firstname
+		session[:user_id] = @user.id
+		session[:message] = "Welcome!"
 
-		@user.to_json
-		# redirect '/items'
+		session.to_json
 	end
 end
