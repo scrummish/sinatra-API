8a2f55c9357e22bc9f8b49f4f73fd0e9bef9b67d
Somers Matthews <somersbmatthews@uchicago.edu>
2018-02-11 11:23:10 -0600

    added relations with user_id in incidents

---
+++ controllers/IncidentController.rb
@@ -0,0 +1,39 @@
+class IncidentController < ApplicationController
+
+	get '/myIncidents' do
+		@user = User.find session[:user_id]
+		@incidents_for_one_civilian = User.incidents.order[:id]
+		response = {
+			status: {
+				all_good: true,
+				number_of_results: @incidents_for_one_civilian.length
+			},
+			incidents: @incidents_for_one_civilian
+		}
+		response.to_json
+
+	end
+
+	get '/allIncidents' do
+		@incidents_for_all_civilians = Incident.all.order[:id]
+
+	end
+
+
+	post '/newIncident' do
+		payload = params
+		payload = JSON.parse(request.body.read).symbolize_keys
+
+		@incident = Incedent.new
+		@incident.type_of_incident     = payload[:type_of_incident]
+		@incident.location_description = payload[:location_description]
+		@incident.address              = payload[:address]
+		@incident.on_going             = payload[:on_going]
+
+		session[:message] = "You added an incident #{@incident.id}"
+
+	end
+
+
+
+end
--- db/migrations.sql
+++ db/migrations.sql
@@ -1,15 +1,27 @@
--- DROP DATABASE snitchr;
--- CREATE DATABASE snitchr;
+DROP DATABASE snitchr;
+CREATE DATABASE snitchr;
 
--- \c snitchr
+\c snitchr
 
--- CREATE TABLE users (
---   id SERIAL PRIMARY KEY,
---   firstname VARCHAR(60),
---   lastname VARCHAR(60),
---   email VARCHAR(60),
---   password_digest VARCHAR(60)
--- );
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  firstname VARCHAR(60),
+  lastname VARCHAR(60),
+  email VARCHAR(60),
+  password_digest VARCHAR(60)
+);
 
--- INSERT INTO users (firstname, lastname, email, password)
--- VALUES ('value1','value2','value3','value4');
+CREATE TABLE incidents (
+	id SERIAL PRIMARY KEY,
+	user_id SERIAL,
+	type_of_incident VARCHAR(60),
+	location_description VARCHAR(60),
+	address VARCHAR(60),
+	on_going BOOL NOT NULL
+);
+
+INSERT INTO users (firstname, lastname, email, password_digest)
+VALUES ('value1','value2','value3','value4');
+
+INSERT INTO incidents (user_id, type_of_incident, location_description, address, on_going)
+VALUES ('1', 'crime', 'at the TJ Max at Madison and State', '11 N. State St., Chicago, IL', true);
