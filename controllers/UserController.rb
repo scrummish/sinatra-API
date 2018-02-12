73fc4b56db96b118b7bd11287418aaacb9c3f229
sergio <scrummish@gmail.com>
2018-02-12 11:06:47 -0600

    Added register form validation

--- controllers/UserController.rb
+++ controllers/UserController.rb
@@ -30,21 +30,41 @@ class UserController < ApplicationController
 		session.to_json
 	end
 
+	# post '/edit' do
+	# 	payload = params
+	# 	payload = JSON.parse(request.body.read).symbolize_keys
+
+	# 	if @user = User.where(email => payload[:email]).first and @user.id != session[:user_id]
+	# 		session[:message] = "Email already exists"
+	# 		p "testing if used email true"
+	# 	elsif @user = User.where(username => payload[:username]).first and @user.username != session[:username]
+	# 		session[:message] = "Username already exists"
+	# 		p "testing if used username true"
+	# 	else
+	# 		@user = User.find_by(id: payload[:email])
+	# 	end
+	# end
+
 	post '/' do
 		payload = params
 		payload = JSON.parse(request.body.read).symbolize_keys
 
-		@user = User.new
-		@user.firstname = payload[:firstname]
-		@user.lastname = payload[:lastname]
-		@user.email = payload[:email]
-		@user.password = payload[:password]
-		@user.save
-		session[:logged_in] = true
-		session[:username] = @user.firstname
-		session[:user_id] = @user.id
-		session[:message] = "Welcome!"
+		if User.find_by(email: payload[:email])
+			@message = "false"
+			@message.to_json
+		else
+			@user = User.new
+			@user.firstname = payload[:firstname]
+			@user.lastname = payload[:lastname]
+			@user.email = payload[:email]
+			@user.password = payload[:password]
+			@user.save
+			session[:logged_in] = true
+			session[:username] = @user.firstname
+			session[:user_id] = @user.id
+			session[:message] = "Welcome!"
 
-		session.to_json
+			session.to_json
+		end
 	end
 end
